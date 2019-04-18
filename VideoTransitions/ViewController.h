36df53f74150c5d3d94245c9235fe59e4c344f12
dgpgith <977539726@qq.com>
2019-04-18 11:35:19 +0800

    Add source

---
+++ VideoTransitions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoTransitions
+//
+//  Created by Charles on 2019/4/18.
+//  Copyright © 2019 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoTransitions/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  VideoTransitions
+//
+//  Created by Charles on 2019/4/18.
+//  Copyright © 2019 Charles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
